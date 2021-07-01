43bc900fb476ba98b4a82229b957d3023c64bd9c
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-07-02 01:28:50 +0300

    сделано топорно

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,25 @@
+import listCount from './templates/countriesMarc.hbs';
+import countrie from './templates/countMarc.hbs';
+// const { alert, notice, info, success, error } = require('@pnotify/core');
+
+const countrieInput = document.querySelector('.label-input');
+const countriesList = document.querySelector('.list-countries');
+
 export default function fetchCountries(searchQuery) {
-    
+    return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+        .then(resp => { return resp.json() })
+        .then(countries => {
+            if (countries.length >= 2 && countries.length <= 10) {
+                const listCountMarc = listCount(countries);
+                return countriesList.insertAdjacentHTML('beforeend', listCountMarc);
+            } else if (countries.length === 1) {
+                const countrieMarc = countrie(countries);
+                
+                return countriesList.insertAdjacentHTML('beforeend', countrieMarc);
+            };
+
+// const myInfo = info({
+//   text: "Too many matches found. Please enter a more specific query!"
+// });
+         }).catch(error => console.log(error));
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import './sass/main.scss';
-import './fetchCountries.js';
+import fetchCountries from './fetchCountries.js';
+var debounce = require('lodash.debounce');
 
-const { alert, notice, info, success, error } = require('@pnotify/core');
-
-const myInfo = info({
-  text: "Too many matches found. Please enter a more specific query!"
-});
 
 const countrieInput = document.querySelector('.label-input');
-const countriesList = document.querySelector('.list-countries');
+let textValue = '';
+function onInput() {
+    textValue = countrieInput.value;
+    return fetchCountries(textValue);
+     
+};
+
+countrieInput.addEventListener('input', debounce(onInput, 500));
+
+
+
+// fetchCountries(textValue);
+// const myError = error({
+//   text: "I'm an error message."
+// });
+// const mySuccess = success({
+//   text: "I'm a success message."
+// });
+// const myNotice = notice({
+//   text: "I'm a notice."
+// });
+// const myInfo = info({
+//   text: "Too many matches found. Please enter a more specific query!"
+// });
+
+
+
+// const countriesList = document.querySelector('.list-countries');
 
