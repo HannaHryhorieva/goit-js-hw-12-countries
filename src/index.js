f07bb8df6af56de56d5f9cf1bf29f0b034e69d91
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-07-01 20:15:47 +0300

    One

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './sass/main.scss';
+import './fetchCountries.js';
+
+const { alert, notice, info, success, error } = require('@pnotify/core');
+
+const myInfo = info({
+  text: "Too many matches found. Please enter a more specific query!"
+});
+
+const countrieInput = document.querySelector('.label-input');
+const countriesList = document.querySelector('.list-countries');
+
