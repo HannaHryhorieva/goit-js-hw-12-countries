05345ed2e69e2dbeeb894efa8ea81e3e7d518f88
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-07-04 00:21:27 +0300

    12 ready

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,14 +1,15 @@
 import listCount from './templates/countriesMarc.hbs';
 import countrie from './templates/countMarc.hbs';
-// const { alert, notice, info, success, error } = require('@pnotify/core');
+const { alert, notice, info, success, error } = require('@pnotify/core');
 
-const countrieInput = document.querySelector('.label-input');
+
 const countriesList = document.querySelector('.list-countries');
 
 export default function fetchCountries(searchQuery) {
     return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
         .then(resp => { return resp.json() })
         .then(countries => {
+            countriesList.innerHTML = '';
             if (countries.length >= 2 && countries.length <= 10) {
                 const listCountMarc = listCount(countries);
                 return countriesList.insertAdjacentHTML('beforeend', listCountMarc);
@@ -17,9 +18,9 @@ export default function fetchCountries(searchQuery) {
                 
                 return countriesList.insertAdjacentHTML('beforeend', countrieMarc);
             };
-
-// const myInfo = info({
-//   text: "Too many matches found. Please enter a more specific query!"
-// });
+            const myAlert = alert({
+            text: "Too many matches found. Please enter a more specific query!",
+            type: 'info'
+});
          }).catch(error => console.log(error));
 }
--- src/index.js
+++ src/index.js
@@ -1,35 +1,24 @@
 import './sass/main.scss';
 import fetchCountries from './fetchCountries.js';
+
+import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+  import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+  defaultModules.set(PNotifyMobile, {});
+  alert({
+    text: 'Too many matches found. Please enter a more specific query!'
+  });
+
 var debounce = require('lodash.debounce');
 
 
 const countrieInput = document.querySelector('.label-input');
 let textValue = '';
 function onInput() {
+
     textValue = countrieInput.value;
     return fetchCountries(textValue);
      
 };
 
 countrieInput.addEventListener('input', debounce(onInput, 500));
-
-
-
-// fetchCountries(textValue);
-// const myError = error({
-//   text: "I'm an error message."
-// });
-// const mySuccess = success({
-//   text: "I'm a success message."
-// });
-// const myNotice = notice({
-//   text: "I'm a notice."
-// });
-// const myInfo = info({
-//   text: "Too many matches found. Please enter a more specific query!"
-// });
-
-
-
-// const countriesList = document.querySelector('.list-countries');
 
